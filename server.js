c5f0356e3d16d4fc9cfe818522e324951c432401
oz-om <ozyoom@gmail.com>
2023-01-23 14:53:36 +0000

    set headers

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use(
   }),
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", ["http://127.0.0.1:5173", clientUrl]);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 app.use(express.json());
