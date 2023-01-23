fc135378a5a6bba8b1cdbebf7251afd3aeae7be8
oz-om <ozyoom@gmail.com>
2023-01-23 16:59:08 +0000

    fine v4?

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ app.use(
     credentials: true,
   }),
 );
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, application/json");
-  next();
-});
 
 app.use(express.json());
 app.use(cookieParser());
