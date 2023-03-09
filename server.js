6cabc5b7f5150d79180ce3015165de221cdf3d22
oz-om <ozyoom@gmail.com>
2023-03-09 22:21:30 +0100

    setup upload

--- server.js
+++ server.js
@@ -3,8 +3,12 @@ const app = express();
 const cors = require("cors");
 const cookie = require("cookie-parser");
 require("dotenv").config();
+const filesUpload = require("express-fileupload");
+const axios = require("axios");
+const BodyForm = require("form-data");
 
 const clientUrl = process.env.CLIENT_URL;
+console.log(clientUrl);
 app.use(
   cors({
     origin: clientUrl,
@@ -13,6 +17,7 @@ app.use(
   cookie(),
   express.json(),
   express.urlencoded({ extended: true }),
+  filesUpload(),
 );
 app.set("trust proxy", 1);
 
@@ -26,6 +31,47 @@ app.get("/", isUser, (req, res) => {
   } else {
     res.send({
       login: false,
+    });
+  }
+});
+
+app.post("/upload", async (req, res) => {
+  if (!req.files) {
+    res.status(500).json({
+      upload: false,
+      msg: "pleas select an image to upload",
+    });
+    return;
+  }
+
+  const img = req.files.file;
+  const form = new BodyForm();
+
+  form.append("file", img.data, {
+    filename: img.name,
+    contentType: img.mimetype,
+    knownLength: img.size,
+  });
+
+  try {
+    const data = await axios({
+      url: "https://telegra.ph/upload",
+      method: "POST",
+      headers: {
+        ...form.getHeaders(),
+      },
+      data: form,
+    });
+    res.status(200).json({
+      upload: true,
+      msg: "upload succefully",
+      imgUrl: "https://telegra.ph" + data.data[0].src,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      upload: false,
+      msg: "Error uploading file",
     });
   }
 });
